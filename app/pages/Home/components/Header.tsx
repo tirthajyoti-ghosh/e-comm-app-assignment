9432dd5986923da856225f93833506398dbb449e
Tirthajyoti Ghosh <itirthahere@gmail.com>
2023-12-14 16:21:33 +0530

    add Header styles in Home screen

--- app/pages/Home/components/Header.tsx
+++ app/pages/Home/components/Header.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
 import Cart from 'app/components/Cart';
+import * as Colors from 'app/styles/colors';
+import * as Typography from 'app/styles/typography';
 
 export default function Header() {
     return (
-        <View>
-            <Text>Hey, Rahul</Text>
+        <View style={styles.container}>
+            <Text style={styles.text}>Hey, Rahul</Text>
             <Cart />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        paddingVertical: 20,
+        paddingHorizontal: 15,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: Colors.primary.b1,
+    },
+    text: {
+        color: Colors.neutral.n7,
+        ...Typography.heading.h3_regular_20,
+    },
+});
