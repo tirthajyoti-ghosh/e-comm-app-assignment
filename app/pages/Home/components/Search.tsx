9e4d2e25826a167fdcc637566067bc931e86df81
Tirthajyoti Ghosh <itirthahere@gmail.com>
2023-12-14 16:37:43 +0530

    add styles to Search component

--- app/pages/Home/components/Search.tsx
+++ app/pages/Home/components/Search.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { View, TextInput } from 'react-native';
+import { View, TextInput, StyleSheet } from 'react-native';
 import SVGSearch from 'app/assets/icons/search.svg';
+import * as Colors from 'app/styles/colors';
 
 export default function Search() {
     return (
-        <View>
+        <View style={styles.container}>
             <SVGSearch />
-            <TextInput placeholder="Search Products or store" />
+            <TextInput style={styles.input} placeholder="Search Products or store" placeholderTextColor="#8891A5" />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 30,
+        paddingVertical: 5,
+        backgroundColor: Colors.primary.b2,
+        borderRadius: 28,
+    },
+    input: {
+        flex: 1,
+        marginLeft: 10,
+        color: Colors.neutral.n7,
+    },
+});
