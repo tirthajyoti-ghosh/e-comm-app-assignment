bc3f9407582d294a2ae38779b5bc616ca8f2ac18
Tirthajyoti Ghosh <itirthahere@gmail.com>
2023-12-14 16:20:59 +0530

    update cart icon styles

--- app/components/Cart.tsx
+++ app/components/Cart.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import SVGBag from 'app/assets/icons/bag.svg';
 import * as Colors from 'app/styles/colors';
+import Position from 'app/styles/position';
 
 export default function Cart() {
     const count = 3;
@@ -9,7 +10,7 @@ export default function Cart() {
         <View>
             {count > 0 && (
                 <View style={styles.count}>
-                    <Text style={{ color: '#fff', fontSize: 10 }}>{count}</Text>
+                    <Text style={styles.countText}>{count}</Text>
                 </View>
             )}
             <SVGBag />
@@ -21,13 +22,18 @@ const styles = StyleSheet.create({
     container: {},
     count: {
         position: 'absolute',
-        top: -5,
-        right: -5,
-        width: 15,
-        height: 15,
+        zIndex: Position.zIndex.overlayLevel99,
+        top: -8,
+        right: -8,
+        width: 20,
+        height: 20,
         borderRadius: 15,
         backgroundColor: Colors.secondary.y1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    countText: {
+        color: '#fff',
+        fontSize: 10,
     },
 });
